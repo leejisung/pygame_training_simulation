db4d954cfb9f2ed31470a1e457f7926f3d545339
leejisung <starseed13@hanmail.net>
2021-01-06 20:47:49 +0900

    스테이터스 창

--- 시험용 매인창 표현/ddd.py
+++ 시험용 매인창 표현/ddd.py
@@ -18,7 +18,7 @@ pg.display.update()
 def update():
     pg.display.update()
 
-class icon():
+class icon_making():
     def __init__(self, img, xy):
         self.img = img
         self.xy = xy
@@ -29,34 +29,55 @@ class icon():
         board.blit(self.img, self.xy)
     def click_event(self, event):
         self.event = event
-class window(icon):
+class window_making(icon_making):
     def blit(self):
         global buttons
         self.cxy = (self.x+240,self.y)
         board.blit(self.img, self.xy)
-        close = icon(close_button, self.cxy)
+        close = icon_making(close_button, self.cxy)
         close.event = close_window
         buttons.append(close)
         close.blit()
-        
+class data_making():
+    def __init__(self):
+        self.hp = 10
+        self.strength = 5
+        self.tec = 5
+        self.stress = 10
+        self.dung = 50
+        self.day = 1
 
 def board_start():
     board.blit(dark,(0,0))
     status_button.blit()
 def status_window_open():
-    status_window.blit()
+    white_window.blit()
+    name = font.render("상태창",True,(255,255,255))
+    hp = font.render("HP: "+str(data.hp),True,(0,0,0))
+    strength = font.render("힘: "+str(data.strength),True,(0,0,0))
+    tec = font.render("기술: "+str(data.tec),True,(0,0,0))
+    stress = font.render("스트레스: "+str(data.stress),True,(0,0,0))
+    dung = font.render("분충도: "+str(data.dung),True,(0,0,0))
+    board.blit(name,(20,20))
+    board.blit(hp,(40,60))
+    board.blit(strength,(40,80))
+    board.blit(tec,(40,100))
+    board.blit(stress,(40,120))
+    board.blit(dung,(40,140))
 def close_window():
     global buttons
     del buttons[-1]
     board_start()
-
-status_button= icon(status_button_img, (330,20))
+data = data_making()
+status_button= icon_making(status_button_img, (330,20))
 status_button.event = status_window_open
-status_window = window(base_window, (20,20))
+white_window = window_making(base_window, (20,20))
   
 ################
 board_start()
-#status_window_open()
+
+
+
 update()
 
 buttons =[status_button]
