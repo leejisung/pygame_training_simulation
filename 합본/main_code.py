1e164cb66c8632fdd65ada04033619c1a19c20a0
leejisung <starseed13@hanmail.net>
2021-01-07 11:23:10 +0900

    대충 합쳐놈

--- 합본/main_code.py
+++ 합본/main_code.py
@@ -1,17 +1,22 @@
 import pygame as pg
 import pandas as pd
 import numpy as np
+import random
 
 
 pg.init()
 font =pg.font.Font('maple.ttf', 20)
-dark = pg.image.load("dark_500_500.png")
+talk_data = pd.read_csv('대사집.csv', encoding='CP949')
 
+
+dark = pg.image.load("dark_500_500.png")
 status_button_img = pg.image.load("see_status.png")
 talk_button_img = pg.image.load("talk.png")
 plan_button_img = pg.image.load("make_plan.png")
 base_window = pg.image.load("base_window.png")
 close_button = pg.image.load("close_button.png")
+text_window = pg.image.load("white_460_110.png")
+
 
 board = pg.display.set_mode((500,500))
 board.blit(dark,(0,0))
@@ -19,6 +24,31 @@ pg.display.update()
 
 def update():
     pg.display.update()
+
+def list_talk(name): 
+    talk= list(talk_data[name]) 
+    while(talk[-1] ==0): 
+        del talk[-1] 
+    return talk
+
+def blit_text(surface, text, font):
+    color=pg.Color('black')
+    surface.blit(text_window, (20, 370))
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+    space = font.size(' ')[0]  # The width of a space.
+    max_width, max_height = (450, 450)
+    x, y = (30, 380)
+    for line in words:
+        for word in line:
+            word_surface = font.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = 30
+                y += word_height  # Start on new row.
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = 30
+        y += word_height  # Start on new row.
 
 class icon_making():
     def __init__(self, img, xy):
@@ -68,6 +98,18 @@ def status_window_open():
     board.blit(tec,(40,100))
     board.blit(stress,(40,120))
     board.blit(dung,(40,140))
+def talk_with_wiki():
+    global talk
+    if data.dung<30:
+        xx= list_talk("30under")
+    elif data.dung>79:
+        xx = list_talk("80over")
+    else:
+        xx = list_talk("30_80")
+    xx= random.choice(xx)
+    blit_text(board, xx, font)
+    talk = True
+    
 def close_window():
     global buttons
     del buttons[-1]
@@ -78,17 +120,28 @@ plan_button= icon_making(plan_button_img, (330,70))
 talk_button= icon_making(talk_button_img, (330,120))
 
 status_button.event = status_window_open
+talk_button.event = talk_with_wiki
+
 white_window = window_making(base_window, (20,20))
   
 ################
 board_start()
 update()
-
+text = []
+talk = False
 buttons =[status_button, talk_button, plan_button]
             
 while True:
     for event in pg.event.get():
         if event.type == pg.MOUSEBUTTONDOWN:
+            if talk == True:
+                if text==[]:
+                    board_start()
+                    update()
+                    talk = False
+                else:
+                    blit_text(board, text[0], font)
+                
             mouse = pg.mouse.get_pos()
             for button in buttons:
                 if button.col.collidepoint(mouse):
